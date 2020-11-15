1f7fccad7746d92455c67acc305145bc2eb1c96f
AleksandraOla1 <ochodun@gmail.com>
2020-11-15 10:57:21 +0000

    1

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+using namespace std;
+
+class Figura
+{
+public:
+    //  void   SetP(double P) { pole = P; };
+    // double getP() { return pole; };
+    void id() { cout << "figura::: :" << endl; }
+
+protected:
+    double pole;
+};
+
+class Kolo : public Figura
+{
+public:
+    Kolo(double r) : Figura{}
+    {
+        pole = 2 * 3.14 * r;
+        cout << "pole kola to: " << pole << endl;
+    }
+    void id() { cout << "kolo, pole to :" << pole << endl; }
+};
+class Kwadrat : public Figura
+{};
 
 int main()
 {
-    puts("Hello, World!");
+    puts("zad 8");
+    Kolo pierwsze(1);
+    pierwsze.id();
 }
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -1,9 +1,35 @@
 #pragma once
+using namespace std;
 
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+ if (towar ==0)
+ {
+   cout<<"nie tworzymy zadnego statku"<<endl;
+ }
+ else
+ {    
+   int i=0;
+   int suma = 0;
+   int towarstatku;
+   int liczbazaglowcow;
+   while (suma < towar)
+   {
+    Stocznia stocznia{};
+    Statek* s1=stocznia();
+    towarstatku = unsigned int transportuj();
+    suma=suma+towarstatku;
+    if(dynamic_cast<Zaglowiec*>(s1))
+    {i=i+1;};
+    
+    delete s1;
+
+   }
+ 
+ }
+ 
+    return i;
 }
